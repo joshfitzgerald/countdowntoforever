d0542ad8e987b9858b1f616b389df301e9a4b541
Joshua Fitzgerald <joshua.fitzgerald@slickdeals.net>
2024-05-07 12:57:51 -0700

    Creates a web socket server and client

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const http = require("http"); 
+const http = require("http");
 http.createServer(function (req, res) {
 	res.write("On my way to being a full stack engineer!");
 	res.end();
 }).listen(3000);
 
 console.log("Server started on port 3000");
-
